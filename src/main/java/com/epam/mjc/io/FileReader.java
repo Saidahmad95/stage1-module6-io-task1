29ff6b4eabea2355612cdd3ef46a5098b254ea59
Saidahmad95 <murtazoyev00@gmail.com>
2022-11-08 19:31:21 +0500

    second

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,46 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public Profile getDataFromFile(File file) throws FileNotFoundException {
+        Profile profile = new Profile();
+
+        try (java.io.FileReader fileReader = new java.io.FileReader(file)) {
+            int c;
+            String s = "";
+            while ((c = fileReader.read()) != -1) {
+                char c1 = (char) c;
+                if (c1 != '\r') {
+                    if (c1 != '\n')
+                        s += c1;
+                    else {
+                        profileMapper(profile, s);
+                        s = "";
+                    }
+                }
+                System.out.print((char) c);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return profile;
     }
+
+    private void profileMapper(Profile profile, String line) {
+        if (line.startsWith("Name")) {
+            profile.setName(line.substring("Name: ".length()));
+        } else if (line.startsWith("Age")) {
+            profile.setAge(Integer.valueOf(line.substring("Age: ".length())));
+        } else if (line.startsWith("Email")) {
+            profile.setEmail(line.substring("Email: ".length()));
+        } else {
+            profile.setPhone(Long.valueOf(line.substring("Phone: ".length())));
+        }
+    }
+
 }
